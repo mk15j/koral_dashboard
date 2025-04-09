a764935eec7b7df90c558336d5a9c7886cd6b9cb
mk15j <161435539+mk15j@users.noreply.github.com>
2025-04-09 14:52:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 
 import streamlit as st
+st.set_page_config(
+    page_title="Koral Listeria Dashboard",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 
 if "user" not in st.session_state:
     st.warning("Please login first.")
@@ -9,11 +14,6 @@ if st.sidebar.button("Logout"):
     st.session_state.clear()
     st.experimental_rerun()
     
-st.set_page_config(
-    page_title="Koral Listeria Dashboard",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
 
 st.title("Welcome to Koral Seafood Listeria Dashboard")
 st.markdown("Use the sidebar to navigate between different dashboard sections.")
