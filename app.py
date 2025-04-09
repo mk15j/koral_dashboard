8b4e296b22cbfc4d66e49f6d0d13b81cdfb0951c
mk15j <161435539+mk15j@users.noreply.github.com>
2025-04-09 14:45:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ if "user" not in st.session_state:
     st.warning("Please login first.")
     st.stop()
     
+if st.sidebar.button("Logout"):
+    st.session_state.clear()
+    st.experimental_rerun()
+    
 st.set_page_config(
     page_title="Koral Listeria Dashboard",
     layout="wide",
